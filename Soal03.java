f230b0904550091ad8ca0bdc496b0ee928278b3f
Rashyyy <rasyanugraha95@gmail.com>
2024-10-29 17:05:51 +0700

    ngerjain soal3

--- Soal03.java
+++ Soal03.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class Soal03 {
 
 	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
+		Scanner input = new Scanner(System.in);
+
+		int bilanganJam = input.nextInt();
+		int bilanganMenit = input.nextInt();
+		int bilanganMenitTambah = input.nextInt();
+
+		int hasil1 = bilanganMenit + bilanganMenitTambah;
+		System.out.println((bilanganJam) + ":" + hasil1);
+
+		int hasil2 = bilanganMenit + (30 - bilanganMenitTambah);
+		System.out.println(( 1 + bilanganJam) + ":" + hasil2);
 	}
 }
